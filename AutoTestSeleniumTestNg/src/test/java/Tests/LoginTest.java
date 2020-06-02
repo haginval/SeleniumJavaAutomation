f8a4055d8480467cef89a3e5d42c7827722e515e
haginval <raghuhk@hotmail.com>
2020-06-02 10:17:19 +0530

    First commit

---
+++ AutoTestSeleniumTestNg/src/test/java/Tests/LoginTest.java
@@ -0,0 +1,22 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	WebDriver driver;
+	
+	@Test
+	public void loginToApp()
+	{
+		System.out.println("this is a test to check Login page functionality");
+		System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://google.com");
+		System.out.println("Launched application successfully.");
+		
+	}
+
+}
